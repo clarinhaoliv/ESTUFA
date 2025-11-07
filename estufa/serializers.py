478893787b5d5440977d0a7f621ca83a94255362
Maria Clara <claraoliv241@gmail.com>
2025-11-07 13:48:26 -0300

    first commit

---
+++ estufa/serializers.py
@@ -0,0 +1,21 @@
+#serializers: componentes fundamentais
+# que convertem dados complexos e formatos simples
+# que podem ser recebidos em APIs REST
+# converter objetos Python em formato nativo (JSON, XML)
+# para envios de respostas HTTP
+
+from rest_framework import serializers
+from .models import Leitura, Alerta
+
+class LeituraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Leitura
+        fields = ['id', 'temperatura', 'umidade', 'data_hora']
+
+class AlertaSerializer(serializers.ModelSerializer):
+    tipo_display = serializers.CharField(source='get_tipo_display', read_oly = True)
+
+    class Meta:
+        model = Alerta
+        fields = ['id', 'tipo', 'tipo_display', 'mensagem', 'data_hora', 'lido']
+        
